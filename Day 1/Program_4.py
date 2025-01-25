da3126bcde243d89bdcbaa52084826ea0d7234f1
OmarGudi <omar.sanchez8409@alumnos.udg.mx>
2025-01-25 12:05:22 -0600

    Program 4 done and staring the number 5.

---
+++ Day 1/Program_4.py
@@ -0,0 +1,9 @@
+name = "Omar"
+print(len(name))
+name = "Paulina"
+print(len(name))
+
+print(len(input("What is your name: ")))
+user_Name = input("Enter your name: ")
+length_Name = len(user_Name)
+print(length_Name)
