83490a015bc30f278032b9be99df6916548a66c6
OmarGudi <omar.sanchez8409@alumnos.udg.mx>
2025-01-25 12:20:50 -0600

    First day completed.

---
+++ Day 1/FinalProject_Program_5.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator Project!")
+city_Name = input("What city did you grew up in? \n: ")
+pet_Name = input("What's the name of your pet?\n: ")
+brand_Name = city_Name + pet_Name
+print(brand_Name)
